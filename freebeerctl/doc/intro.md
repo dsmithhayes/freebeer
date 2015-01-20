322f18d090e74fc0d65f85d00d665340121c9b33
Dave Smith-Hayes <dsmithhayes@gmail.com>
2015-01-20 14:52:01 -0500

    Starting the freebeerctl.

---
+++ freebeerctl/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to freebeerctl
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ freebeerctl/src/freebeerctl/core.clj
@@ -0,0 +1,10 @@
+; This program will check the current working directory
+; for music files and then add them to a torrent file.
+
+(ns freebeerctl.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
